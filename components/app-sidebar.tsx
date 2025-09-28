db69f0e37cf6092f3978cf4eff523608f04fad21
Iacopo Paolucci <paolucciacopo@gmail.com>
2025-09-29 01:50:43 +0200

    some changes

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -30,8 +30,8 @@ import {
 
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "Iacopo Paolucci",
+    email: "paolucciacopo@gmail.com",
     avatar: "/avatars/shadcn.jpg",
   },
   navMain: [
